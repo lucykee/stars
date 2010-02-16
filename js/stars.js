a7351fc3e0590c98204198e8a3a3bf9b84d9ff1f
Lucy Kee <lucy.kee@gmail.com>
2010-02-15 19:45:24 -0800

    make stars grow

--- js/stars.js
+++ js/stars.js
@@ -1,10 +1,10 @@
-jQuery(function() {
+$(function() {
     $(window).click(function(e) {
         var x = e.clientX;
         var y = e.clientY;
-        // var $star = jQuery('body').append('<span class="star">♥</span>');
-        var $star = jQuery('<span class="star">♥</span>');
-        jQuery('body').append($star);
+        // var $star = $('body').append('<span class="star">♥</span>');
+        var $star = $('<span class="star">♥</span>');
+        $('body').append($star);
 
         $star.css('color', 'red');
         $star.css({
@@ -14,12 +14,15 @@ jQuery(function() {
         });
 
         $star
-            .bind('mousedown', function() {
-                jQuery(this).css('color', 'green');
-                return false;
-            })
             .bind('mouseup', function() {
-                jQuery(this).css('color', 'blue');
+		var size = 100;
+	        var $star = $(this);
+		setInterval(
+		    function() {
+			size = size + 5;
+                        $star.css('font-size', String(size) + '%');
+		    }, 100
+		);
                 return false;
             })
             .bind('click', function() {
